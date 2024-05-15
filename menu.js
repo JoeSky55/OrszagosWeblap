e861eb3b8ec56153f15b6dfbb26eeb659507ab93
JoeSky55 <gal.jozsef.1037@dszcbaross.edu.hu>
2024-05-15 11:17:45 +0200

    hatterek

--- menu.js
+++ menu.js
@@ -7,6 +7,9 @@ var menu =
           </button>
           <div class="collapse navbar-collapse" id="collapsibleNavbar">
             <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="https://hu.wikipedia.org/wiki/Jap%C3%A1n"><img src="japan_flag_icon.jpg" alt="" style="width: 40px"></a>
+              </li>
               <li class="nav-item">
                 <a class="nav-link" href="hokkaido.html">Hokkaido</a>
               </li>
