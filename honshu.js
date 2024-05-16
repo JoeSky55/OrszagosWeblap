bc1f9251866136742116557440271aa4de7f6fda
JoeSky55 <gal.jozsef.1037@dszcbaross.edu.hu>
2024-05-16 10:43:53 +0200

    utolsomentes

--- honshu.js
+++ honshu.js
@@ -1,24 +1,44 @@
-var honshu_adatok = {
-    "main_cities": [
+var honshu_adatok = [
       {
-        "city": "Tokyo",
-        "population": 13929286
+        "name": "Fushimi-Inari szentély",
+        "picture_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3D%2522fushimi%2Binari%2Bshrine%2522&psig=AOvVaw0Tinh-AKOsqb4vheRmaCbh&ust=1715934039546000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCID6weHekYYDFQAAAAAdAAAAABAE"
       },
       {
-        "city": "Yokohama",
-        "population": 3726167
+        "name": "Senso-ji",
+        "picture_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3Dsenso-ji&psig=AOvVaw1Hs_kGRrsl-9klKGJ0TDvV&ust=1715934070768000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCKiYyfHekYYDFQAAAAAdAAAAABAE"
       },
       {
-        "city": "Osaka",
-        "population": 2691179
+        "name": "Tokyo Tower",
+        "picture_url": "https://i.pinimg.com/originals/c1/eb/47/c1eb470a07ec726e87331b598e69f9ac.jpg"
       },
       {
-        "city": "Nagoya",
-        "population": 2296461
+        "name": "Tokyo Skytree",
+        "picture_url": "https://cdn.britannica.com/09/193909-050-5804497B/photo-Tokyo-distance-right-Sky-Tree-image.jpg"
       },
       {
-        "city": "Sapporo",
-        "population": 1952876
+        "name": "Osakai várkastély",
+        "picture_url": "https://t4.ftcdn.net/jpg/00/90/08/33/360_F_90083377_p9e5sPwqQxE3qP13FM9hgy6D47SiPin4.jpg"
+      },
+      {
+        "name": "Arashiyama babuszerdő",
+        "picture_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiQXOTGtSVBRJRiQOQ1CC6CpVfTLBhoZw44rIYhgR4rA&s"
+      },
+      {
+        "name": "Himeji várkastély",
+        "picture_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeAwJP-kIXdhcLDCXj0N1AeKBKZE4-3H-TLMeslDxODw&s"
+      },
+      {
+        "name": "Hiroshima Béke emlékmű",
+        "picture_url": "https://assets.evcdn.net/pim-assets-images/27063/5df8d44a9e74a.jpeg"
       }
     ]
-  }
+    var nevezetessegek = ""
+    for (const elem of honshu_adatok) {
+        
+       
+        
+    }
+
+    document.getElementById("carousel-kepek").innerHTML=nevezetessegek
+  
+  
